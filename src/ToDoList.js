75b89f038df1cd90855d61f08581863482aedd3c
Frank <flin1105@gmail.com>
2023-02-01 15:40:27 -0500

    Adding feature to allow user to add to do items

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -7,14 +7,38 @@ function ToDoList() {
 		"Delete all the useless items",
 	]);
 
+	const [newItem, addNewItem] = useState("");
+
+	const handleText = (e) => {
+		addNewItem(e.target.value);
+	};
+
+	function handleOnSubmit(event) {
+		event.preventDefault();
+		console.log("item", newItem);
+		setList([...list, newItem]);
+	}
+
 	return (
-		<ul>
-			{list.map((item, index) => (
-				<li key={index}>
-					<p>{item}</p>
-				</li>
-			))}
-		</ul>
+		<>
+			<ul>
+				{list.map((item, index) => (
+					<li key={index}>
+						<p>{item}</p>
+					</li>
+				))}
+			</ul>
+			<form onSubmit={handleOnSubmit}>
+				<input
+					onChange={handleText}
+					type="onSubmit"
+					name="newItem"
+					placeholder="Please enter a new to do item"
+					value={newItem}
+				></input>
+				<button>Add Item to List</button>
+			</form>
+		</>
 	);
 }
 
